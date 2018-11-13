66fc1d4c04abea9e449c77b67ada1e7b7ba25b8a
Unknown <ramon.caballeroiv@gmail.com>
2018-11-12 17:25:06 -0700

    Fixed issue where the skeleton in PlaybackEditScene would not move

---
+++ Assets/Scripts/Helpers/PlaybackEditSetup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaybackEditSetup : MonoBehaviour {
+
+    public NativeAvatar PlaybackAvatar;
+    public Playback Playback;
+
+	void Start () {
+		PlaybackAvatar.SwapSkeletonProvider(Playback);
+	}
+}
