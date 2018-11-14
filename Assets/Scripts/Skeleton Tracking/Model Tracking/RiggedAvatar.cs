c10ac8f12342a673d163b0943bc26f57c61fd29c
Unknown <ramon.caballeroiv@gmail.com>
2018-11-14 13:25:09 -0700

    Added Avatar class to move character model. Only translates the model in space.

---
+++ Assets/Scripts/Skeleton Tracking/Model Tracking/RiggedAvatar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using nuitrack;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class RiggedAvatar : MonoBehaviour, IAvatar {
+
+    public ISkeletonProvider SkeletonProvider { get; protected set; }
+
+    void Start() {
+        SkeletonProvider = CurrentUserTracker.Instance;
+    }
+
+    void FixedUpdate() {
+        ProcessSkeleton(SkeletonProvider.CurSkeleton);
+    }
+
+    public void SwapSkeletonProvider(ISkeletonProvider newProvider) {
+        SkeletonProvider = newProvider;
+    }
+
+    private void ProcessSkeleton(Skeleton s) {
+        Vector3 torsoPos = Quaternion.Euler(0f, 180f, 0f) * (0.001f * s.GetJoint(JointType.Torso).ToVector3());
+        transform.position = torsoPos;
+    }
+}
