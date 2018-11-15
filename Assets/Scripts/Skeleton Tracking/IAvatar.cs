8027c49f4ee72681739ba699de1691225ee5da7e
Unknown <ramon.caballeroiv@gmail.com>
2018-11-15 11:03:04 -0700

    Added methods to set the color of limbs directly to the Avatar classes instead of being spread out

--- Assets/Scripts/Skeleton Tracking/IAvatar.cs
+++ Assets/Scripts/Skeleton Tracking/IAvatar.cs
@@ -1,6 +1,11 @@
-﻿public interface IAvatar {
+﻿using nuitrack;
+using UnityEngine;
+
+public interface IAvatar {
 
     ISkeletonProvider SkeletonProvider { get; }
 
     void SwapSkeletonProvider(ISkeletonProvider newProvider);
+    void SetColor(JointType jointType, Color color);
+    void SetColor(ComparisonFrameData comparison);
 }
