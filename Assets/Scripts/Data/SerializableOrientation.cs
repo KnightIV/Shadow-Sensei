624f9d267fb7bd7f7447a9272e5ea6d858477937
Unknown <ramon.caballeroiv@gmail.com>
2018-11-15 16:31:29 -0700

    Created SerializableOrientation to allow for serializing Orient of a Joint

---
+++ Assets/Scripts/Data/SerializableOrientation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using nuitrack;
+
+[Serializable]
+public struct SerializableOrientation {
+
+    public float[] Matrix;
+
+    public static implicit operator SerializableOrientation(Orientation o) {
+        return new SerializableOrientation {
+            Matrix = (float[]) o.Matrix.Clone()
+        };
+    }
+
+    public static implicit operator Orientation(SerializableOrientation o) {
+        return new Orientation {
+            Matrix = (float[]) o.Matrix.Clone()
+        };
+    }
+}
