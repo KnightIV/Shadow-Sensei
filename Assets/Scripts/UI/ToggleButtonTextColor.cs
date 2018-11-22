0d5781c84809d5930fa5bc60a42dd0453e6449dc
Unknown <ramon.caballeroiv@gmail.com>
2018-11-21 17:14:50 -0700

    Made script to change the color of a button's text based on if it's highlighted or not

---
+++ Assets/Scripts/UI/ToggleButtonTextColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+using TextMeshProUGUI = TMPro.TextMeshProUGUI;
+
+public class ToggleButtonTextColor : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler {
+
+    public Color DefaultColor, HighlightedColor, ClickedColor;
+
+    private TextMeshProUGUI buttonText;
+    
+	void Start () {
+	    buttonText = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+	}
+
+    public void OnPointerDown(PointerEventData eventData) {
+        buttonText.color = HighlightedColor;
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        buttonText.color = DefaultColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        buttonText.color = ClickedColor;
+    }
+}
