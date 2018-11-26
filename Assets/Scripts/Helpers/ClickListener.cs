0992d864271c3974468add260b9e53c495ec5c1b
Unknown <ramon.caballeroiv@gmail.com>
2018-11-26 13:23:35 -0700

    Added ClickListener to detect left and right clicks

---
+++ Assets/Scripts/Helpers/ClickListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickListener : MonoBehaviour, IPointerClickHandler {
+
+    public delegate void OnClick();
+
+    public event OnClick OnRightClick, OnLeftClick;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        switch (eventData.button) {
+            case PointerEventData.InputButton.Right:
+                OnRightClick?.Invoke();
+                break;
+
+            case PointerEventData.InputButton.Left:
+                OnLeftClick?.Invoke();
+                break;
+        }
+    }
+}
