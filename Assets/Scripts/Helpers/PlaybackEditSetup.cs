2a293f3c32d470c389e774ccd4dd796303484eb4
Ramon Caballero <ramon.caballeroiv@gmail.com>
2019-05-27 17:12:51 -0600

    Added logic to populate username field if the user is logged in.

--- Assets/Scripts/Helpers/PlaybackEditSetup.cs
+++ Assets/Scripts/Helpers/PlaybackEditSetup.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaybackEditSetup : MonoBehaviour {
 
     public NativeAvatar PlaybackAvatar;
     public RiggedAvatar RiggedPlaybackAvatar;
     public Playback Playback;
+    [SerializeField] private InputField usernameInput;
 
 	void Start () {
         IAvatar playbackAvatar = RiggedPlaybackAvatar as IAvatar ?? PlaybackAvatar;
         playbackAvatar.SwapSkeletonProvider(Playback);
         Playback.Reset();
 	}
+
+    public void EditTextInput() {
+        if (VariableHolder.User.IsLoggedIn()) {
+            usernameInput.text = VariableHolder.User.Username;
+            usernameInput.interactable = false;
+        }
+    }
 }
