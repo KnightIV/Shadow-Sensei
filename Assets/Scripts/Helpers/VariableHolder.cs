5cdf0d9cdcf37c263e8b129f64a01a1700a060f4
Ramon Caballero <ramon.caballeroiv@gmail.com>
2019-05-28 12:56:57 -0600

    Hard-coded user info for debugging

--- Assets/Scripts/Helpers/VariableHolder.cs
+++ Assets/Scripts/Helpers/VariableHolder.cs
@@ -6,8 +6,14 @@ public static class VariableHolder {
     public static string TechniqueToDelete;
 
     public static class User {
-        public static string Username;
-        public static string Password;
+
+        //public static int UserID;
+        //public static string Username;
+        //public static string Password;
+
+        public static int UserID = 5;
+        public static string Username = "KnightIV";
+        public static string Password = "pass";
 
         public static bool IsLoggedIn() {
             return !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password);
